d19450230c024f80a49603957a742c5d782504d4
Mike Carbonari <mikecarbonari1@gmail.com>
2022-07-03 23:17:48 -0500

    last commit of day

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 `use-strict`;
 $(document).ready(function () {
+  /** DOM Elements */
   const navbar = document.getElementById('navbar');
+
+  /** Event Listensers */
   $(window).scroll(function () {
     const top = navbar.getBoundingClientRect().top;
     top == 0 ? (navbar.style.visibility = 'visible') : (navbar.style.visibility = 'hidden');
