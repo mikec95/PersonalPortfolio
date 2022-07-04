969638ba9e8316483034d95be4ff9138b3635daf
Mike Carbonari <mikecarbonari1@gmail.com>
2022-07-03 19:34:06 -0500

    Change styles and a whole bunch of other stuff

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
+`use-strict`;
 $(document).ready(function () {
-  // Add smooth scrolling to all links
-  $('a').on('click', function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== '') {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      var hash = this.hash; // Store hash to be used later in adding to the URL
-
-      /**
-       * Scroll for 800ms
-       */
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    }
+  const navbar = document.getElementById('navbar');
+  $(window).scroll(function () {
+    const top = navbar.getBoundingClientRect().top;
+    top == 0 ? (navbar.style.visibility = 'visible') : (navbar.style.visibility = 'hidden');
   });
 });
